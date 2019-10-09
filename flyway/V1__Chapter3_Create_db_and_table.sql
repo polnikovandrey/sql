ef5d422fdda99f361c506c14f32cef0d3f91d1d5
Andrey Polnikov <polnikov.andrey@gmail.com>
2019-10-09 21:43:37 +0300

    Chapter 3 Creating databases and tables ..1

--- flyway/V1__Chapter3_Create_db_and_table.sql
+++ flyway/V1__Chapter3_Create_db_and_table.sql
@@ -1,7 +1,45 @@
 # $mysql-ctl cli;           # Start the CLI (command line interface)
-CREATE DATABASE soap_shop;  # Create database with name
-USE soap_shop;              # Select database for use
-SELECT DATABASE();          # Show database currently used
-DROP DATABASE soap_shop;    # Delete database by name
-SELECT DATABASE();          # Note empty result after deletion of currently used database
-SHOW DATABASES;             # List available (to logged user) databases
+
+# Create database with name
+CREATE DATABASE soap_shop;
+# Select database for use
+USE soap_shop;
+# Show database currently used
+SELECT DATABASE();
+# Delete database by name
+DROP DATABASE soap_shop;
+# Note empty result after deletion of currently used database
+SELECT DATABASE();
+# List available databases (available to logged user)
+SHOW DATABASES;
+
+
+CREATE DATABASE cats_app;
+USE cats_app;
+# Create a table with some columns. Note: plural noun recommended for table name.
+CREATE TABLE cats(
+    name VARCHAR(100),
+    age INT
+);
+# Show tables in actual database
+SHOW TABLES;
+# Show columns of a table
+SHOW COLUMNS FROM cats;
+# Show columns of a table (another way)
+DESC cats;
+# Delete table in actual database
+DROP TABLE cats;
+DROP DATABASE cats_app;
+
+CREATE DATABASE pastry_shop;
+SHOW DATABASES;
+USE pastry_shop;
+SELECT DATABASE();
+CREATE TABLE pastries(
+    name VARCHAR(50),
+    quantity INT
+);
+SHOW TABLES;
+SHOW COLUMNS FROM pastries;
+DROP TABLE pastries;
+DROP DATABASE pastry_shop;
