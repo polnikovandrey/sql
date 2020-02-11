f9fb1afee6039734d587fa0b6bf73b0f71531c1c
Andrey Polnikov <polnikov.andrey@gmail.com>
2020-02-11 22:08:52 +0300

    Chapter 16 Introducing Node ..4

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => {
     res.send('Server response\n');
 });
 app.listen(3000);
-console.log(Date.now() + 'Running on http://localhost:3000');
+console.log(Date.now() + ' Running on http://localhost:3000');
