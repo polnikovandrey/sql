bd84c6490dfa539a80ebb85c2fd14bd0d25c9e8f
Andrey Polnikov <polnikov.andrey@gmail.com>
2020-02-16 23:52:11 +0300

    Chapter 16 Introducing Node ..7

--- nodejs/dev.js
+++ nodejs/dev.js
@@ -1,1 +1,11 @@
-console.log(Date.now() + 'Running dev script');
+'use strict';
+console.log(new Date() + ': Running dev script');
+const faker = require('faker');
+
+generateAddress();
+
+function generateAddress() {
+    console.log(faker.address.streetAddress());
+    console.log(faker.address.city());
+    console.log(faker.address.state());
+}
