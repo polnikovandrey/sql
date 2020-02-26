33bfaa0aed2d8ed867f42ec1cb5edf99458245bd
Andrey Polnikov <polnikov.andrey@gmail.com>
2020-02-26 23:51:13 +0300

    Chapter 16 Introducing Node ..8

---
+++ flyway/V14__Chapter16_Introducing_node.sql
@@ -0,0 +1,2 @@
+# This user with legacy auth is needed for nodejs mysql lib to authenticate mysql >8.0 server. Modern authentication was not supported.
+CREATE USER 'legacy_auth_user' IDENTIFIED WITH mysql_native_password BY 'legacy_auth_path';
--- nodejs/dev.js
+++ nodejs/dev.js
@@ -1,11 +1,32 @@
 'use strict';
 console.log(new Date() + ': Running dev script');
 const faker = require('faker');
-
-generateAddress();
-
+function divider(functionName, start) {
+    if (start) console.log('');
+    console.log('---- ' + (start ? 'Start of ' : 'End of ') + functionName + ' ----')
+}
 function generateAddress() {
-    console.log(faker.address.streetAddress());
+    divider('generateAddress()', true);
+    console.log(faker.address.streetAddress(false));
     console.log(faker.address.city());
     console.log(faker.address.state());
+    divider('generateAddress()', false);
 }
+generateAddress();
+
+const mysql = require('mysql');
+// Mysql server >8.0 authentication was not supported. Using legacy auth. See V14__Chapter16_Introducing_node.sql comment.
+let connection = mysql.createConnection({
+    host: 'mysql',
+    user: 'legacy_auth_user',
+    password: 'legacy_auth_path'
+});
+function firstQuery(connection) {
+    connection.query('SELECT 2 + 2 AS solution', function (error, results) {
+        if (error) throw error;
+        divider('firstQuery()', true);
+        console.log('The solution of 2 + 2 is ' + results[0].solution);
+        divider('firstQuery()', false);
+    });
+}
+firstQuery(connection);
